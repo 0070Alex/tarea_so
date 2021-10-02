db0b7b17e89aaea25b1420bd57862e31422553a2
0070Alex <d700.elias64gmai.com>
2021-10-02 14:29:07 -0600

    Se realizaron cambios

--- src/App.js
+++ src/App.js
@@ -8,12 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Docker-Meetup</h1>
+          <h1 className="App-title">Bienvenidos a Sistemas Operativos</h1>
         </header>
         <p className="App-intro">
-          Welcome to this Docker Meetup with Jenkins
-
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Sistemas Operativos 1 <span>Douglas Alexander</span>
         </p>
       </div>
     );
