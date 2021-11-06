57d7b59b49418f4bd44481cc68f0459a53adaa93
0070Alex <d700.elias64gmai.com>
2021-11-05 21:38:05 -0600

    Guardar datos

--- src/App.js
+++ src/App.js
@@ -1,51 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Category from './Category.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Bienvenidos a Sistemas Operativos</h1>
-        </header>
-        <p className="App-intro">
-          Sistemas Operativos 1 <span>Douglas Alexander Sacalxot Elias</span>
-        </p>
+        <ul>
+          <Category name="Pagina de Inicio" items={['Mision','Vision','Objetivos']}/>
+          <Category name="Productos" items={['Ofertas del Mes','Black Friday','En liquidacion']}/>
+          <Category name="Contacto" items={['Sedes','Ubicacion','Video Informativo']}/>
+        </ul>
       </div>
-      
-      	
-		<h1>Caja 3</h1>
-			<p>En esta caja, que también comienza con un párrafo, seguiremos con una tabla:
-		<table>
-			<caption>Tabla de Ejemplo</caption>
-			<tr>
-				<td class="vacio"></td>
-				<th>izquierda</th>
-				<th>centro</th>
-				<th>derecha</th>
-			</tr>
-			<tr>
-				<th>primero</th>
-				<td>primero-izquierda</td>
-				<td>primero-centro</td>
-				<td>primero-derecha</td>
-			</tr>
-			<tr>
-				<th>segundo</th>
-				<td>segundo-izquierda</td>
-				<td>segundo-centro</td>
-				<td>segundo-derecha</td>
-			</tr>
-			<tr>
-				<th>tercero</th>
-				<td>tercero-izquierda</td>
-				<td>tercero-centro</td>
-				<td>tercero-derecha</td>
-			</tr>
-		</table>
-	
     );
   }
 }
