555fa2e5e9ef95a0f775650b0e945f5cca9fce2e
0070Alex <d700.elias@gmail.com>
2021-11-11 18:16:21 -0600

    Nosotros

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,18 +3,18 @@ const Home = () => {
 	return (
         <div className="sec__one">
 			<h1> Nosotros </h1>
-				<table width="60%" align="center">
+			<table width="60%" align="center">
 		<tr style="font-style: bold">
 			<td align="center">Misión</td>
 			
 		</tr>
 		<tr>
 			<td>
-				<hr>
-				Formar, en niveles de excelencia académica y de acuerdo con las necesidades y demandas de la sociedad, profesionales, investigadores, técnicos y docentescientíficamente competentes, éticamente responsables y socialmente abiertos al cambio progresista, mediante el desarrollo integrado de la docencia, la investigación y el servicio a la comunidad, a fin que ésta tenga la oportunidad de participar de los beneficios de la ciencia, la tecnología y la cultura.
-				<br>
-				Asimismo, que sus graduados sean capaces de conservar, acrecentar y difundir la cultura universal, afirmando los valores propios e inmersos en el proceso productivo del país, y que contribuyan con espíritu crítico a la solución de los problemas nacionales.
-				<br><br><br><tr><br><br><br>	
+				<hr/>
+				Formar, en niveles de excelencia académica y de acuerdo con las necesidades y demandas de la sociedad, profesionales, investigadores, técnicos y docentescientíficamente 				competentes, éticamente responsables y socialmente abiertos al cambio progresista, mediante el desarrollo integrado de la docencia, la investigación y el servicio a la 					comunidad, a fin que ésta tenga la oportunidad de participar de los beneficios de la ciencia, la tecnología y la cultura.
+				<br/>
+				Asimismo, que sus graduados sean capaces de conservar, acrecentar y difundir la cultura universal, afirmando los valores propios e inmersos en el proceso productivo del 				país, y que contribuyan con espíritu crítico a la solución de los problemas nacionales.
+				<br/><br/><br/><tr/><br/><br/><br/>	
 			</td>		
 
 		</tr>
@@ -24,11 +24,11 @@ const Home = () => {
 		<tr>
 			
 			<td>
-				<hr>
-				Ser una institución de educación superior altamente competitiva, líder en su género, innovadora y creadora de conocimientos científicos y tecnológicos, en la que sus educandos alcancen una formación integral, armónica y potencien sus aptitudes y capacidades, en un ambiente de libertad y aceptación de los supremos valores del cristianismo.
-				<br>
-				Y que sus egresados, sean capaces de asumir los desafíos del mundo contemporáneo y de una sociedad cambiante, comprometidos con el desarrollo económico, social, político y cultural del país, en permanente interacción con el entorno nacional, con renovada vocación de servicio y espíritu de solidaridad.
-				<br><br><br><tr><br><br><br>	
+				<hr/>
+				Ser una institución de educación superior altamente competitiva, líder en su género, innovadora y creadora de conocimientos científicos y tecnológicos, en la que sus 					educandos alcancen una formación integral, armónica y potencien sus aptitudes y capacidades, en un ambiente de libertad y aceptación de los supremos valores del 				cristianismo.
+				<br/>
+				Y que sus egresados, sean capaces de asumir los desafíos del mundo contemporáneo y de una sociedad cambiante, comprometidos con el desarrollo económico, social, 				político y cultural del país, en permanente interacción con el entorno nacional, con renovada vocación de servicio y espíritu de solidaridad.
+				<br/><br/><br/><tr/><br/><br/><br/>	
 			</td>
 		</tr>
 		<tr align="center">
@@ -42,7 +42,7 @@ const Home = () => {
 			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. 
-			<br>
+			<br/>
 			Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
