618a1c8553fff70180d18d95117b518b516a65fa
0070Alex <d700.elias@gmail.com>
2021-11-11 19:05:03 -0600

    Servicios

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,28 @@ import React from 'react'
 const About = () => {
        return (
               <div className="sec__one">
-                     <h1> About </h1>
+                     <table width="50%" align="center">
+	<tr>
+		<td><h1 align="center">Carreras</h1></td>
+	</tr>
+	<tr>
+		<td>
+			<ul>
+				<li>ARQUITECTURA</li>
+				<li>CIENCIAS DE LA COMUNICACIÓN</li>
+				<li>CIENCIAS ECONÓMICAS</li>
+				<li>CIENCIAS JURÍDICAS Y SOCIALES</li>
+				<li>CIENCIAS MÉDICAS Y DE LA SALUD</li>
+				<li>CIENCIAS QUÍMICAS y BIOLÓGICAS</li>
+				<li>HUMANIDADES</li>
+				<li>INGENIERIA EN SISTEMAS</li>
+				<li>ODONTOLOGÍA</li>
+				<li>PSICOLOGÍA</li>
+				<li>TEOLOGÍA</li>
+			</ul>
+		</td>
+	</tr>
+</table>
               </div>
        )
 }
